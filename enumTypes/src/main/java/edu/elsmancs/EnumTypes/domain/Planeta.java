41703dbeca14a8940f94ffbcb0149d4a070f13ce
Albertomanas <amanas@cifpfbmoll.eu>
2020-04-01 16:35:49 +0200

    feat(Planeta): enum Planeta, propiedades y private Planeta
    Hecha la estructura básica de las variables que se utilizarán en la
    fórmula

--- enumTypes/src/main/java/edu/elsmancs/EnumTypes/domain/Planeta.java
+++ enumTypes/src/main/java/edu/elsmancs/EnumTypes/domain/Planeta.java
@@ -1,4 +1,33 @@
 package edu.elsmancs.EnumTypes.domain;
 
-public class Planeta {
+import java.util.EnumSet;
+
+//Libro Beginning Java 8 Fundamentals (Cap. 18 Enum Types)
+
+public enum Planeta {
+
+    // pag 707 (Declaración de enum)
+    MERCURY (3.303e+23, 2.4397e6),
+    VENUS (4.869e+24, 6.0518e6),
+    EARTH (5.976e+24, 6.37814e6),
+    MARS (6.421e+23, 3.3972e6),
+    JUPITER (1.9e+27, 7.1492e7),
+    SATURN (5.688e+26, 6.0268e7),
+    URANUS (8.686e+25, 2.5559e7),
+    NEPTUNE (1.024e+26, 2.4746e7);
+
+    private double masa = 0d;
+    private double radio = 0d;
+
+
+    /**propiedad de la gravedad G (kg-1 y s-2) Teoría de la gravitación universal
+     *  G= (6,674*10^-11)* ((N*m^2)/kg^2)
+     */
+    public static final double G = 6.67300e-11;
+
+    private Planeta(double masa, double radio) {
+        this.masa = masa;
+        this.radio = radio;
+    }
+
 }
